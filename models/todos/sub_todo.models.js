076738c7842ff66b335fb7937f42790c58d357a5
Rohan801651 <rohaanali82005@gmail.com>
2024-09-18 17:20:18 +0500

    sub_todo

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose"
+import { type } from "os";
 
-const subTodoSchema = new mongoose.Schema({}, {timestamps: true});
+const subTodoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    complete: {
+        type: Boolean,
+        default: false,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    
+}, {timestamps: true});
 
 export const subTodo = mongoose.model("SubTodo", subTodoSchema)
