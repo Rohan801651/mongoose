6931b50a99c7e63d2d550699f7c378298d2fc3b9
Rohan801651 <rohaanali82005@gmail.com>
2024-09-18 16:40:04 +0500

    ref of other model

--- models/todos/todo.models.js
+++ models/todos/todo.models.js
@@ -1,5 +1,26 @@
 import mongoose from "mongoose"
 
-const todoSchema = new mongoose.Schema({}, {timestamps: true});
+const todoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    complete: {
+        type: Boolean,
+        default: false,
+    },
+    createdBy: {
+        // reference of other model
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User",
+    },
+    subTodos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SubTodo",
+        }
+    ] // array of sub TOdos
+
+}, {timestamps: true});
 
 export const todo = mongoose.model("Todo", todoSchema)
